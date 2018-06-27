46ca2abd6f6d1f525b15395c4d5801d6cfd70623
Peter Mackenzie-Helnwein <mackheln@gmail.com>
2018-06-27 00:04:57 -0700

    More improvements to makeAnim.sh

--- src/makeAnim.sh
+++ src/makeAnim.sh
@@ -2,8 +2,15 @@
 
 CONVERT=/opt/local/bin/convert
 
-${CONVERT} -delay 20 Pressure*png AnimPressure.gif
-${CONVERT} -delay 20 Stream*png   AnimStream.gif
-${CONVERT} -delay 20 Forces*png   AnimForces.gif
-${CONVERT} -delay 20 Velocity*png AnimVelocity.gif
-${CONVERT} -delay 20 ParticleVelocity*png AnimParticles.gif
+if [ -x ${CONVERT} ]
+then
+	${CONVERT} -delay 20 Pressure*png AnimPressure.gif
+	${CONVERT} -delay 20 Stream*png   AnimStream.gif
+	${CONVERT} -delay 20 Forces*png   AnimForces.gif
+	${CONVERT} -delay 20 Velocity*png AnimVelocity.gif
+	${CONVERT} -delay 20 ParticleVelocity*png AnimParticles.gif
+else
+	echo "Install the ImageMagick package and adapt this script to locate convert"
+	exit 1
+fi
+
