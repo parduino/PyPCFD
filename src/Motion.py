5aada7f5d79b6ad08cbce3f3437a384dae5c4c0c
krishnendu <kshekhar@uw.edu>
2019-08-09 15:45:01 -0700

    fixed errors in motion 2

--- src/Motion.py
+++ src/Motion.py
@@ -42,30 +42,27 @@ class Motion1(Motion):
         theta = pi
         self.X0 = array([0.5, 0.5])
 
-        self.Vel0 = array([0.1, 0.0])  # translation velocity
+        self.Vel0 = array([0.1, 0.0]) *0.0 # translation velocity
 
         # calculate rotation matrix
         self.Omega = array([[0.0, -theta],
                             [theta, 0.0]])  # skew symmetric matrix
 
-        # ############
-        # self.Vel0 = array([0.1, 0.0])  # translation velocity
-        #
-        # self.Omega = array([[0.0, -theta],
-        #                     [theta, 0.0]])  # skew symmetric matrix
-        # #############
-
     def __str__(self):
         return "motion_1"
 
     def getVel(self, xIJ, time):
-        return self.Omega @ (xIJ - self.X0 - time * self.Vel0) + self.Vel0
+        v = self.Omega @ (xIJ - self.X0 - time * self.Vel0) + self.Vel0
+        # print(v)
+        return v
 
     def getDvDt(self, xIJ, time):
         return -self.Omega @ self.Vel0
 
     def getAnalyticalF(self, time):
-        return expm(time * self.Omega)  # brute force matrix exponential
+        Q = expm(time * self.Omega)
+        print(Q)
+        return Q  # brute force matrix exponential
 
     def getAnalyticalPosition(self, x0, time):
         Q = expm(time * self.Omega)
@@ -77,19 +74,24 @@ class Motion2(Motion):
 
     def __init__(self):
         super().__init__()
-        # set global motion parameters
-        # gg = 0.5
-        # gamma = [gg, 1.-gg]
-        # omega =
 
-        self.gamma1 = 1.0
+        # self.gamma1 = 0.5
+        # self.gamma2 = 1. - self.gamma1
+        #
+        # self.Omega1 = array([[0., -1.], [1., 0.]])*pi
+        # self.Omega2 = array([[0., -1.], [1., 0.]])/2.
+        #
+        # self.X1 = array([0.5, 0.5])
+        # self.X2 = array([0.8, 0.8])
+
+        self.gamma1 = 0.1
         self.gamma2 = 1. - self.gamma1
 
         self.Omega1 = array([[0., -1.], [1., 0.]])*pi
-        self.Omega2 = array([[0., -1.], [1., 0.]])/2.
+        self.Omega2 = array([[0., -1.], [1., 0.]])*pi/3
 
         self.X1 = array([0.5, 0.5])
-        self.X2 = array([10., 10.])
+        self.X2 = array([0.5, 0.5])
 
         self.x0 = self.gamma1 * self.X1 + self.gamma2 * self.X2
 
@@ -115,6 +117,8 @@ class Motion2(Motion):
         v1 = self.Omega1 @ (S1 @ (xIJ + xTilde) - x1)
         v2 = self.Omega2 @ (S2 @ (xIJ + xTilde) - x2)
         v = self.gamma1 * v1 + self.gamma2 * v2
+
+        # print(v)
 
         return v
 
@@ -144,6 +148,7 @@ class Motion2(Motion):
         dvdt = self.gamma1 * (self.Omega1 @ v1) + self.gamma2 * (self.Omega2 @ v2)
         dvdt -= dot(gradv, self.getVel(xIJ, time))
 
+        # print(dvdt)
         return dvdt
 
 
@@ -152,18 +157,16 @@ class Motion2(Motion):
         Q2 = expm(time*self.Omega2)
 
         R = self.gamma1 * Q1 + self.gamma2 * Q2
+        # print(R)
         return R
 
     def getAnalyticalPosition(self, x0, time):
         Q1 = expm(time*self.Omega1)
         Q2 = expm(time*self.Omega2)
 
-        x1 = Q1 @ self.X1
-        x2 = Q1 @ self.X2
-
-        x = self.gamma1 * Q1 @ (x0 - x1) \
-            + self.gamma2 * Q2 @ (x0 - x2) \
-            + self.x0
+        x = self.gamma1 * Q1 @ (x0 - self.X1) \
+            +self.gamma2 * Q2 @ (x0 - self.X2) \
+            +self.x0
 
         return x
 
