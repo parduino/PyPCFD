23ef6d8c0a6a7a2c292ec19d72f4d3a5d8154fd9
krishnendu <kshekhar@uw.edu>
2019-08-15 18:49:54 -0700

    First implementation of Motion 3.

--- src/testF.py
+++ src/testF.py
@@ -1,32 +1,18 @@
 from LocalConvergenceTest import *
 from GlobalConvergenceTest import *
-from numpy import array, linspace, zeros, zeros_like
+from numpy import array, linspace, zeros, zeros_like, tensordot
 import matplotlib.pyplot as plt
 
 def Main():
     fileType = 'pdf'
     # fileType = 'png'
-    LocalConvergenceTest(fileType).runAnalysis()
+    # LocalConvergenceTest(fileType).runAnalysis()
     # GlobalConvergenceTest(fileType).runAnalysis()
     # Motion1().plotMotion()
     # Motion2().plotMotion()
-
-
-
-#     ax = addData()
-#     ax.scatter(2,3)
-#     matplotlib.rcParams['font.sans-serif'] = "Times New Roman"
-#     matplotlib.rcParams['font.size'] = 15
-#     ax.axis("equal")
-#     ax.grid(True)
-#     plt.show()
-#
-# def addData():
-#     fig, ax = plt.subplots()
-#     ax.scatter(1, 2)
-#     return ax
-
-
+    m = Motion3()
+    m. getLagrangianPosition(array([1., 0.]), 1.0)
+    # m.plotMotion()
 
 if __name__ == '__main__':
     Main()
